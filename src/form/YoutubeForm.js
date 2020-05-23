58c33002cffd066e8933e1e0e1d7eaaebff927df
MARZOUK Soufiane <soufianemarzouk.2017@gmail.com>
2020-05-23 02:33:02 +0100

    wrap error msg in a component

--- src/form/YoutubeForm.js
+++ src/form/YoutubeForm.js
@@ -3,6 +3,7 @@ import {useFormik,Formik,Form,Field,ErrorMessage} from 'formik'
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import * as Yup from 'yup'
+import TextError from '../component/TextError'
 
 
 const useStyles = makeStyles((theme) => ({
@@ -57,25 +58,25 @@ function YoutubeForm() {
                <div className='form-control'>
                 <label htmlFor='name' >Name</label>
                 <Field  type='text' id='name' name='name' />
-                <ErrorMessage name="name" />
+                <ErrorMessage name="name" component={TextError} />
                 </div>
 
                 <div className='form-control'>
                 <label htmlFor='email' >E-Mail</label>
                 <Field  type='email' id='email' name='email'/>
-                <ErrorMessage name="email" />
+                <ErrorMessage name="email" component={TextError}/>
                 </div>
 
                 <div className='form-control'>
                 <label htmlFor='channel' >Channel</label>
-                <Field  type='text' id='channel' name='channel' />
-                <ErrorMessage name="channel" />
+                <Field  type='text' id='channel' name='channel'/>
+                <ErrorMessage name="channel" component={TextError}/>
                 </div>
 
                 <div className='form-control'>
                 <label htmlFor='channel' >Comments</label>
                 <Field  as='textarea' type='text' id='comments' name='comments' />
-                <ErrorMessage name="comments" />
+                <ErrorMessage name="comments" component={TextError}/>
                 </div>
 
                 <div className='form-control'>
